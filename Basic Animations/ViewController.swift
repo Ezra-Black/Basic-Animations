a48cafd4979007df88e53564700aeac31163e626
Joseph Rogers <joerogersss2016@gmail.com>
2019-11-14 02:33:18 -0600

    Initial Commit

---
+++ Basic Animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Basic Animations
+//
+//  Created by Joseph Rogers on 11/14/19.
+//  Copyright © 2019 Joseph Rogers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
